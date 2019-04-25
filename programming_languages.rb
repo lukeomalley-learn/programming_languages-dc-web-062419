dcae0ffd7720859dd3713ca7340f9a6ea29fefa1
Luke O'Malley <lukeomalley95@gmail.com>
2019-04-25 14:47:58 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,7 +6,7 @@ def reformat_languages(languages)
       if language_hash.has_key?(:language)
         language_hash[language][:language_style] << language_style
       else
-        language_hash[language] = {type: language_type}
+        language_hash[language] = language_type
         language_hash[language][:style] = [language_style]
       end
     end
