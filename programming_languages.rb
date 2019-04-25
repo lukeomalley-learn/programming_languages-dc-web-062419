76c629def7bdc952be2be9995f6031087a6ebe17
Luke O'Malley <lukeomalley95@gmail.com>
2019-04-25 14:41:45 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,46 @@
 def reformat_languages(languages)
-  # your code here
+  language_hash = {}
+  
+  languages.each do |key, languages|
+    languages.each do |language, language_type|
+      if language_hash.has_key?(:style)
+        language_hash[language][:style] << key
+      else
+        language_hash[language] = {type: language_type[:type], style: [key]}
+      end
+    end
+  end
+  return language_hash
 end
+
+puts reformat_languages({
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+})
